0f10c3644ec7048680eb676de2a5f802bd473688
Liam Soutter <lsoutter110@gmail.com>
2023-12-02 17:34:30 +0000

    Added input file automation

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,84 @@
 pub fn main(filename: &str) {
-    println!("{filename}");
+    println!("Part 1:");
+    part1(filename);
+
+    println!("Part 2:");
+    part2(filename);
+}
+
+fn part1(filename: &str) {
+    let file_string = read_to_lines(filename);
+
+    let sum: u32 = file_string.iter().map(|line| {
+        let first = line.chars().fold(None, |s, c| match (s, c) {
+            (Some(n), _) => Some(n),
+            (None, n @ '0'..='9') => Some(n.to_digit(10).unwrap()),
+            _ => None,
+        }).unwrap();
+        let last = line.chars().rev().fold(None, |s, c| match (s, c) {
+            (Some(n), _) => Some(n),
+            (None, n @ '0'..='9') => Some(n.to_digit(10).unwrap()),
+            _ => None,
+        }).unwrap();
+        return 10*first + last;
+    }).sum();
+
+    println!("Sum of values = {sum}");
+}
+
+fn part2(filename: &str) {
+    let digit_strs = vec![(0,"zero"), (1,"one"), (2,"two"), (3,"three"), (4,"four"), (5,"five"), (6,"six"), (7,"seven"), (8,"eight"), (9,"nine")];
+    let file_string = read_to_lines(filename);
+
+    // //screen for strings
+    // for line in &mut file_string {
+        
+    //     for line_i in 0..line.len() {
+    //         for di in 0..digit_strs.len() {
+    //             let dlen = digit_strs[di].len();
+    //             if &line[line_i..std::cmp::min(line_i+dlen, line.len())] == digit_strs[di] {
+    //                 line.replace_range(line_i..=line_i, &format!("{di}"));
+    //             }
+    //         }
+    //     }
+    // }
+
+    let sum: u32 = file_string.iter()
+        .map(|line| {
+            let line = digit_strs.iter().fold(line.to_string(), |l, (i, n)| l.replace(n, &format!("{n}{i}{n}")));
+            // println!("{}", line);
+            let first = line.chars().fold(None, |s, c| match (s, c) {
+                (Some(n), _) => Some(n),
+                (None, n @ '0'..='9') => Some(n.to_digit(10).unwrap()),
+                _ => None,
+            }).unwrap();
+            let last = line.chars().rev().fold(None, |s, c| match (s, c) {
+                (Some(n), _) => Some(n),
+                (None, n @ '0'..='9') => Some(n.to_digit(10).unwrap()),
+                _ => None,
+            }).unwrap();
+
+            return 10*first + last;
+        })
+        .sum();
+
+    println!("Sum of values = {sum}");
+}
+
+fn read_to_lines(filename: &str) -> Vec<String> {
+    use std::{fs::File, path::Path, io::Read};
+    let path = Path::new(filename);
+
+    let mut file = match File::open(path) {
+        Ok(f) => f,
+        Err(e) => panic!("Couldn't open {}: {}", filename, e),
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Ok(_) => (),
+        Err(e) => panic!("Couldn't read {} to string: {}", filename, e),
+    }
+
+    return s.split("\n").map(|line| line.to_string()).collect();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ mod day2;
 fn main() {
     let mut args = std::env::args();
     let _ = args.next();
-    
+
     let day = match args.next() {
         Some(s) => s,
         None => { println!("Argument error: Expected day argument (1-25)!"); return },
     };
+
+    let test = day.chars().last() == Some('t');
+    let day_end = if test { day.len()-1 } else { day.len() };
+
     let file = match args.next() {
         Some(s) => s,
-        None => { println!("Argument error: Expected filename argument"); return },
+        None => format!("input/day{}/{}.txt", &day[..day_end], if test { "test" } else { "input" }),
     };
 
-    match &day[..] {
+    match &day[..day_end] {
         "1" => day1::main(&file[..]),
         "2" => day2::main(&file[..]),
         d => println!("Argument error: Unknown day '{d}'"),
