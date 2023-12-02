92d1477847cb58ff85a39888466914d38a7ecbd6
Liam Soutter <lsoutter110@gmail.com>
2023-12-02 17:11:09 +0000

    Added runtime framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod day1;
+mod day2;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+    let _ = args.next();
+    
+    let day = match args.next() {
+        Some(s) => s,
+        None => { println!("Argument error: Expected day argument (1-25)!"); return },
+    };
+    let file = match args.next() {
+        Some(s) => s,
+        None => { println!("Argument error: Expected filename argument"); return },
+    };
+
+    match &day[..] {
+        "1" => day1::main(&file[..]),
+        "2" => day2::main(&file[..]),
+        d => println!("Argument error: Unknown day '{d}'"),
+    }
 }
