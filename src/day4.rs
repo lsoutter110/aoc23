a7a305afce7033c535019c17fa34ae60d0b1a659
Liam Soutter <lsoutter110@gmail.com>
2023-12-05 16:15:48 +0000

    Day 4 working

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,7 @@
 use aoc_util::*;
 use prse::{self, Parse, parse};
 use regex::Regex;
+use std::collections::HashMap;
 
 pub fn main(filename: &str) {
     println!("Part 1:");
@@ -29,12 +30,17 @@ fn part2(filename: &str) {
     let cards: Vec<Card> = file::read_to_lines(filename).iter()
         .map(|l| -> Card { parse!(Regex::new(r" +").unwrap().replace_all(l, " "), "{}") })
         .collect();
-    let sum: usize = (0..cards.len()).map(|i| open_cards(&cards, i)).sum();
+    let mut cache: HashMap<usize,usize> = HashMap::new();
+    let sum: usize = (0..cards.len()).map(|i| open_cards(&cards, &mut cache, i)).sum();
     println!("Sum of points is {}", sum);
 }
 
-fn open_cards(cards: &[Card], index: usize) -> usize {
-    let count = cards[index].numbers.iter().filter(|n| cards[index].winning.contains(n)).count();
-    let count2: usize = (index+1..=index+count).map(|i| open_cards(&cards, i)).sum();
-    return count + count2;
+fn open_cards(cards: &[Card], cache: &mut HashMap<usize,usize>, index: usize) -> usize {
+    if let Some(i) = cache.get(&index) {
+        return *i;
+    }
+    let count_match = cards[index].numbers.iter().filter(|n| cards[index].winning.contains(n)).count();
+    let count_cards: usize = (index+1..=index+count_match).map(|i| open_cards(&cards, cache, i)).sum();
+    cache.insert(index, count_cards + 1);
+    return count_cards + 1;
 }
