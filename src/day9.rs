b0ae5625af697c2780faa140075ba5b4b5f86ad3
Liam Soutter <lsoutter110@gmail.com>
2023-12-09 06:11:40 +0000

    Day 9 done

--- src/day9.rs
+++ src/day9.rs
@@ -17,9 +17,33 @@ pub fn main(filename: &str) {
 }
 
 fn part1(filename: &str) -> i64 {
-
+    let seqs: Vec<Vec<i64>> = file::read_to_lines(filename).iter().map(|l| parse!(l, "{: :}")).collect();
+    return seqs.iter().map(get_next).sum();
 }
 
 fn part2(filename: &str) -> i64 {
+    let seqs: Vec<Vec<i64>> = file::read_to_lines(filename).iter().map(|l| parse!(l, "{: :}")).collect();
+    return seqs.iter().map(get_prev).sum();
+}
 
+fn get_next(seq: &Vec<i64>) -> i64 {
+    let mut diff = Vec::new();
+    for i in 0..seq.len()-1 {
+        diff.push(seq[i+1]-seq[i]);
+    }
+    if diff.iter().all(|i| *i == 0) {
+        return seq[0]; // all same val
+    }
+    return seq[seq.len()-1] + get_next(&diff);
+}
+
+fn get_prev(seq: &Vec<i64>) -> i64 {
+    let mut diff = Vec::new();
+    for i in 0..seq.len()-1 {
+        diff.push(seq[i+1]-seq[i]);
+    }
+    if diff.iter().all(|i| *i == 0) {
+        return seq[0]; // all same val
+    }
+    return seq[0] - get_prev(&diff);
 }
